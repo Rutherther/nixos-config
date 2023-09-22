7ec4d0cac278c18283c3f54b31eaeb6d5e5f38d5
Rutherther <rutherther@proton.me>
2023-09-22 23:13:39 +0200

    chore: cleanup commented programs

--- hosts/home.nix
+++ hosts/home.nix
@@ -53,25 +53,13 @@
       # General configuration
       alsa-utils	      # Audio Commands
       git               # Repositories
-      #jq		# JSON processor
       procps            # Stop Applications
-      #pciutils         # Computer Utility Info
       pipewire          # Sound
-      #usbutils         # USB Utility Info
       wacomtablet       # Wacom Tablet
       wget              # Downloader
-      #socat		# Data Transfer
-      #thunar           # File Manager
-      #zsh              # Shell
       #
       # General home-manager
       alacritty         # Terminal Emulator
-      #dunst            # Notifications
-      #libnotify        # Dependency for Dunst
-      #neovim           # Text Editor
-      #rofi             # Menu
-      #rofi-power-menu  # Power Menu
-      #udiskie          # Auto Mounting
       vim               # Text Editor
       #
       # Xorg configuration
@@ -79,64 +67,24 @@
       #xorg.xev         # Input Viewer
       #xorg.xkill       # Kill Applications
       #xorg.xrandr      # Screen Settings
-      #xterm            # Terminal
       #
       # Xorg home-manager
       flameshot         # Screenshot
       picom             # Compositer
       #
-      # Wayland configuration
-      #autotiling       # Tiling Script
-      #eww-wayland	# Bar
-      #grim             # Image Grabber
-      #slurp            # Region Selector
-      #swappy           # Screenshot Editor
-      #swayidle         # Idle Management Daemon
-      #waybar           # Bar
-      #wev              # Input Viewer
-      #wl-clipboard     # Console Clipboard
-      #wlr-randr        # Screen Settings
-      #xwayland         # X for Wayland
-      #
-      # Wayland home-manager
-      #pamixer          # Pulse Audio Mixer
-      #swaylock-fancy   # Screen Locker
-      #
       # Desktop
-      #ansible          # Automation
-      blueman           # Bluetooth
-      #deluge           # Torrents
       discord           # Chat
       telegram-desktop  # Chat
       unstable.cinny-desktop     # Chat
       ffmpeg           # Video Support (dslr)
-      #gmtp             # Mount MTP (GoPro)
-      #gphoto2          # Digital Photography
-      #handbrake        # Encoder
-      #heroic           # Game Launcher
-      #hugo             # Static Website Builder
-      #lutris           # Game Launcher
-      #mkvtoolnix       # Matroska Tool
-      #nvtop            # Videocard Top
-      #plex-media-player# Media Player
-      #prismlauncher    # MC Launcher
-      #steam            # Games
-      #simple-scan      # Scanning
-      #sshpass          # Ansible dependency
-      # 
-      # Laptop
-      #cbatticon        # Battery Notifications
-      #blueman          # Bluetooth
-      #light            # Display Brightness
-      #libreoffice      # Office Tools
-      #simple-scan      # Scanning
-      #
-      #obs-studio       # Recording/Live Streaming
+
       thunderbird       # email client
       spotify
       obsidian        # Text Editor
     ];
+
     file.".config/wall".source = ../modules/themes/wall;
+
     pointerCursor = {                         # This will set cursor system-wide so applications can not choose their own
       gtk.enable = true;
       #name = "Dracula-cursors";
@@ -155,9 +103,7 @@
   gtk = {                                     # Theming
     enable = true;
     theme = {
-      #name = "Dracula";
       name = "Catppuccin-Mocha-Compact-Blue-Dark";
-      #package = pkgs.dracula-theme;
       package = pkgs.catppuccin-gtk.override {
         accents = ["blue"];
         size = "compact";
@@ -169,7 +115,6 @@
       package = pkgs.papirus-icon-theme;
     };
     font = {
-      #name = "JetBrains Mono Medium";
       name = "FiraCode Nerd Font Mono Medium";
     };                                        # Cursor is declared under home.pointerCursor
   };
