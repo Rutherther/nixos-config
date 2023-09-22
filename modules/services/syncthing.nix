e09f66ea4c31e95a365292a03d39ef3ffcda608b
Rutherther <rutherther@proton.me>
2023-09-22 19:00:05 +0200

    fix: add necessary firewall syncthing settings

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -1,6 +1,11 @@
 { config, lib, pkgs, user, ... }:
 
 {
+  networking.firewall = {
+    allowedTCPPorts = [ 22000 ];
+    allowedUDPPorts = [ 22000 21027 ];
+  };
+
   services.syncthing = {
     enable = true;
     user = "${user}";
