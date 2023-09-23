d5cfcb075895406047bcc047222cf5798b2ac54c
Frantisek Bohacek <rutherther@proton.me>
2023-09-23 21:48:42 +0200

    feat: add git user configuration

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -6,6 +6,14 @@
   programs = {
     git = {
       enable = true;
+      includes = [{
+        contents = {
+          user = {
+            email = "rutherther@proton.me";
+            name = "Rutherther";
+          };
+        };
+      }];
     };
   };
 }
