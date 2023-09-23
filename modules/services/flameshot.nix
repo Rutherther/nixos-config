852a5059a5562a63a22325f83a19413eb051b71a
František Boháček <fandabohacek@gmail.com>
2023-09-23 19:57:17 +0200

    feat: ensure ~/screens created

--- modules/services/flameshot.nix
+++ modules/services/flameshot.nix
@@ -4,19 +4,30 @@
 
 { config, lib, pkgs, user, ... }:
 
-{
+let
+  screensDir = "/home/${user}/screens";
+in {
   services = {                                    # sxhkd shortcut = Printscreen button (Print)
     flameshot = {
       enable = true;
       settings = {
         General = {                               # Settings
-          savePath = "/home/${user}/screens";
+          savePath = screensDir;
           saveAsFileExtension = ".png";
           uiColor = "#2d0096";
           showHelp = "false";
           disabledTrayIcon = "true";              # Hide from systray
         };
       };
+    };
+  };
+
+  home.activations = {
+    ensureScreensDirCreated = {
+      after = [ "writeBoundary" ];
+      data = ''
+        mkdir -p ${screensDir}
+      '';
     };
   };
 }
