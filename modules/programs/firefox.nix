9003b5b4d0096648b7764a6939cb05531114d65f
František Boháček <fandabohacek@gmail.com>
2023-09-23 19:52:24 +0200

    feat: add more extensions and settings to firefox

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -33,13 +33,36 @@ in {
           youtube-shorts-block
 
           # Privacy
-          # Don't touch my tabs!
-          # Don't track me google
           link-cleaner
           clearurls
           decentraleyes
           privacy-badger
+          skip-redirect
+
+          # Paywalls
+          # bypass-paywalls-clean
         ];
+
+        settings = {
+          "browser.newtabpage.enabled" = false; # Blank new tab page.
+          "browser.startup.page" = 3; # Resume last session.
+          "browser.tabs.closeWindowWithLastTab" = false;
+          # Fully disable Pocket. See
+          # https://www.reddit.com/r/linux/comments/zabm2a.
+          "extensions.pocket.enabled" = false;
+          "extensions.pocket.api" = "0.0.0.0";
+          "extensions.pocket.loggedOutVariant" = "";
+          "extensions.pocket.oAuthConsumerKey" = "";
+          "extensions.pocket.onSaveRecs" = false;
+          "extensions.pocket.onSaveRecs.locales" = "";
+          "extensions.pocket.showHome" = false;
+          "extensions.pocket.site" = "0.0.0.0";
+          "browser.newtabpage.activity-stream.pocketCta" = "";
+          "browser.newtabpage.activity-stream.section.highlights.includePocket" =
+            false;
+          "services.sync.prefs.sync.browser.newtabpage.activity-stream.section.highlights.includePocket" =
+            false;
+        };
       };
     };
   };
