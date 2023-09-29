d4356a9e42ec3f7f5e04dc4631acb5b2e748bac9
Rutherther <rutherther@proton.me>
2023-09-29 21:36:20 +0200

    fix: correctly split options in xkbOptions

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -7,7 +7,7 @@
 
       layout = "us,cz";                              # Keyboard layout & €-sign
       xkbVariant = ",qwerty";
-      xkbOptions = "grp:alt_shift_toggle ctrl:nocaps";
+      xkbOptions = "grp:alt_shift_toggle, ctrl:nocaps";
       libinput.enable = true;
       modules = [ pkgs.xf86_input_wacom ];        # Both needed for wacom tablet usage
       wacom.enable = true;
