fdb403d166904046711c139cdd82644d05a0442e
Rutherther <rutherther@proton.me>
2023-09-30 22:26:16 +0200

    feat: add laptop to syncthing

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -15,23 +15,24 @@
     devices = {
       "phone" = { id = inputs.semi-secrets.syncthing.devices.phone; };
       "nixos-desktop" = { id = inputs.semi-secrets.syncthing.devices.desktop; };
+      "nixos-laptop" = { id = inputs.semi-secrets.syncthing.devices.laptop; };
     };
     folders = {
       "uni" = {
         path = "/home/${user}/doc/uni";
-        devices = [ "phone" "nixos-desktop" ];
+        devices = [ "phone" "nixos-desktop" "nixos-laptop" ];
       };
       "notes" = {
         path = "/home/${user}/doc/notes/obsidian/Notes";
-        devices = [ "phone" "nixos-desktop" ];
+        devices = [ "phone" "nixos-desktop" "nixos-laptop" ];
       };
       "camera" = {
         path = "/home/${user}/doc/camera";
-        devices = [ "phone" "nixos-desktop" ];
+        devices = [ "phone" "nixos-desktop" "nixos-laptop" ];
       };
       "study" = {
         path = "/home/${user}/doc/study";
-        devices = [ "phone" "nixos-desktop" ];
+        devices = [ "phone" "nixos-desktop" "nixos-laptop" ];
       };
     };
     extraOptions.options = {
