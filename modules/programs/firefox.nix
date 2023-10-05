31f6109fdb1ed38491029746c83b6b3c284c8333
Rutherther <rutherther@proton.me>
2023-10-05 17:01:53 +0200

    feat: enable tridactyl native

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -19,6 +19,9 @@ in {
 
   programs.firefox = {
     enable = true;
+    package = pkgs.firefox.override {
+      cfg = { enableTridactylNative = true; };
+    };
     profiles = {
       default = {
         id = 0;
