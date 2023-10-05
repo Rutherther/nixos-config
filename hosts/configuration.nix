78fb4accddf0dde5ea3ad7af41afd389322eb336
Frantisek Bohacek <rutherther@proton.me>
2023-10-05 19:26:51 +0200

    feat: make ubuntu default monospace

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -77,6 +77,7 @@
     (nerdfonts.override {                   # Nerdfont Icons override
       fonts = [
         "FiraCode"
+        "Ubuntu"
       ];
     })
   ];
@@ -85,7 +86,7 @@
     defaultFonts = {
       serif = [ "Ubuntu" ];
       sansSerif = [ "Ubuntu" ];
-      monospace = [ "FiraCode" ];
+      monospace = [ "Ubuntu Mono" ];
     };
   };
 
