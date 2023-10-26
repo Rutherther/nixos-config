5936b943041d077d0a46052c7f49c63fa2f0baed
Frantisek Bohacek <rutherther@proton.me>
2023-10-26 19:53:19 +0200

    feat: disable tests in qtile-extras

--- modules/desktop/qtile/default.nix
+++ modules/desktop/qtile/default.nix
@@ -5,6 +5,24 @@
     xkblayout-state
   ];
 
+  nixpkgs.overlays = [
+    (final: super: {
+      pythonPackagesOverlays = (super.pythonPackagesOverlays or []) ++ [
+        (_: pprev: {
+          qtile-extras = pprev.qtile-extras.overridePythonAttrs(_: {
+            doCheck = false;
+          });
+        })
+      ];
+
+      python3 = let self = super.python3.override {
+        inherit self;
+        packageOverrides = super.lib.composeManyExtensions final.pythonPackagesOverlays;
+      }; in self;
+      python3Packages = final.python3.pkgs;
+    })
+  ];
+
   services = {
     xserver = {
       enable = true;
