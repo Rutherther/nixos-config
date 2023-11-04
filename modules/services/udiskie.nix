bdcd6182b3255cb884b5cc21c7c3f8de67c2030f
Frantisek Bohacek <rutherther@proton.me>
2023-11-04 09:59:35 +0100

    chore: update

--- modules/services/udiskie.nix
+++ modules/services/udiskie.nix
@@ -2,14 +2,14 @@
 # Mounting tool
 #
 
-{ config, lib, pkgs, ... }:
+{ config, lib, nixpkgs, pkgs, ... }:
 
 {
-  # services = {
-  #   udiskie = {                         # Udiskie wil automatically mount storage devices
-  #     enable = true;
-  #     automount = true;
-  #     tray = "auto";                    # Will only show up in systray when active
-  #   };
-  # };
+  services = {
+    udiskie = {                         # Udiskie wil automatically mount storage devices
+      enable = true;
+      automount = true;
+      tray = "auto";                    # Will only show up in systray when active
+    };
+  };
 }
