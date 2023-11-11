92c4ec73e55ced2a92b361f221727e5375485d21
Frantisek Bohacek <rutherther@proton.me>
2023-11-11 09:06:55 +0100

    chore: remove some game packages

--- modules/programs/games.nix
+++ modules/programs/games.nix
@@ -7,24 +7,14 @@
 
 { config, pkgs, nur, lib, ... }:
 
-let                                             # No longer required because of retroarch but let's keep it for testing purposes
-  pcsx2 = pkgs.pcsx2.overrideAttrs (old: {      # PCSX2 runs way better on x11. This wrappers makes it use the correct GDK Backend
-    nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.makeWrapper ];
-    postFixup = ''
-      wrapProgram $out/bin/pcsx2 \
-        --set GDK_BACKEND x11
-    '';
-  });
-in
 {
   environment.systemPackages = [
     pkgs.lunar-client
-    pkgs.heroic
-    pkgs.lutris
-    pkgs.prismlauncher
-    pkgs.retroarchFull
-    pkgs.steam
-    pcsx2
+    # pkgs.heroic
+    # pkgs.lutris
+    # pkgs.prismlauncher
+    # pkgs.retroarchFull
+    # pcsx2
   ];
 
   programs = {                                  # Needed to succesfully start Steam
