0cf3a637d42c2ea352eb8d30a50eb00b0911b12f
Frantisek Bohacek <rutherther@proton.me>
2024-05-03 18:50:18 +0200

    feat: set new waillpaper fetched from nixos-artwork

--- home/modules/profiles/desktop/qtile/default.nix
+++ home/modules/profiles/desktop/qtile/default.nix
@@ -13,7 +13,10 @@ let
         show_center = "${inputs.self}/scripts/notifications/show-center.sh";
       };
     };
-    wallpaper = "${inputs.self}/wall.png";
+    wallpaper = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/c68a508b95baa0fcd99117f2da2a0f66eb208bbf/wallpapers/nix-wallpaper-nineish-dark-gray.png";
+      hash = "sha256-nhIUtCy/Hb8UbuxXeL3l3FMausjQrnjTVi1B3GkL9B8=";
+    };
     defaults = {
       terminal = config.home-config.defaultTerminal.meta.mainProgram;
     };
